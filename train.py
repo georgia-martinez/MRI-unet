7dfaca462e5b8292273609f68c8dbc5b632a7707
gcm49 <gcm49@lamarr.ebme.cwru.edu>
2022-10-03 22:10:24 -0400

    Created train_val_split method

--- train.py
+++ train.py
@@ -12,15 +12,22 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.callbacks import EarlyStopping, LambdaCallback
 from tensorflow.keras.optimizers import Adam
 
+from validation_set import train_val_split
+
 # Setting up the parser
 parser = argparse.ArgumentParser(description="Train")
 
 parser.add_argument("-m", "--model", type=str, metavar="", help="Name of the model (e.g. average_1)")
 parser.add_argument("-v", "--version", type=str, metavar="", nargs="?", default="", help="Version number of model (e.g. v2 or v2)")
 parser.add_argument("-e", "--experiment", type=str, metavar="", help="Experiment number to access the correct folder")
-parser.add_argument("-f", "--hdf5", type=str, metavar="", help="Experiment number to access the hdf5 files")
+# parser.add_argument("-f", "--hdf5", type=str, metavar="", help="Experiment number to access the hdf5 files")
 
 args = parser.parse_args()
+
+model_name = args.model
+model_version = args.version
+exp_num = args.experiment
+# hdf5_num = args.hdf5
 
 # Important hyperparameters
 image_height = 128
@@ -30,18 +37,13 @@ epochs = 50
 batch_size = 16
 learning_rate = 3e-3
 
-model_name = args.model
-model_version = args.version
-exp_num = args.experiment
-hdf5_num = args.hdf5
-
 full_model_name = model_name + model_version # (e.g. average_1 or average_1v4)
 
 model_out_path = f"/data/gcm49/experiment{exp_num}/models/{full_model_name}.h5" 
 
-# Paths to the training and validation hdf5 files
-HDF5Path_train = f"/data/gcm49/experiment{hdf5_num}/hdf5_files/{model_name}_train.h5"
-HDF5Path_val = f"/data/gcm49/experiment{hdf5_num}/hdf5_files/{model_name}_val.h5"
+# Paths to the training hdf5 file
+HDF5Path_train = f"/data/gcm49/experiment{exp_num}/hdf5_files/{model_name}.h5"
+# HDF5Path_val = f"/data/gcm49/experiment{exp_num}/hdf5_files/{model_name}_val.h5"
 
 # Build the network
 input_img = Input((image_width, image_height, num_channels), name="img")
@@ -70,21 +72,18 @@ early_stop = EarlyStopping(monitor="loss", patience=4, mode="auto", min_delta=0.
 
 callbacks = [early_stop, json_logging_callback]
 
-# Load in training dataset
+# Get the training and validation datasets
 print("Loading the training dataset")
 
 with h5py.File(HDF5Path_train, "r") as f:
-    X_train = f["raw"][()]
-    y_train = f["labels"][()]
+    raw = f["raw"][()]
+    labels = f["labels"][()]
+    names = f["slice_names"][()]
 
-print("Loading the validation dataset")
+print("Creating the training, validation split...")
+X_train, y_train, X_val, y_val = train_val_split(raw, labels, names)
 
-# Load in validation dataset
-with h5py.File(HDF5Path_val, "r") as f:
-    X_val = f["raw"][()]
-    y_val = f["labels"][()]
-
-print("Finished loading the training and validation datasets\n")
+print("Finished loading the training set and validation datasets\n")
 
 # Expanding dimensions from (D, H, W) to (D, H, W, C)
 X_train = np.expand_dims(X_train, 3)
@@ -106,9 +105,6 @@ data_gen_args = dict(rotation_range=30,
 
 image_datagen = ImageDataGenerator(**data_gen_args)
 mask_datagen = ImageDataGenerator(**data_gen_args)
-
-image_datagen.fit(X_train, augment=True, seed=7)
-mask_datagen.fit(y_train, augment=True, seed=7)
 
 image_generater = image_datagen.flow(X_train, batch_size=batch_size, seed=7)
 mask_generator = mask_datagen.flow(y_train, batch_size=batch_size, seed=7)
@@ -133,7 +129,7 @@ for X_batch, y_batch in train_generator:
 # Run U-Net
 print("Running the U-Net")
 
-model.fit(train_generator, validation_data=(X_val, y_val), epochs=epochs, callbacks=callbacks)
+model.fit(train_generator, steps_per_epoch = len(X_train)//batch_size, validation_data=(X_val, y_val), epochs=epochs, callbacks=callbacks)
 
 # model.fit(X_train, y_train, validation_data=(X_val, y_val), batch_size=batch_size, epochs=epochs, callbacks=callbacks)
 
